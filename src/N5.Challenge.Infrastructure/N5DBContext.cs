a7c529a6679c6aef32d91c525c944dcdb06fdd40
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2023-05-27 16:30:08 -0500

    ⚡️Infraestructure

---
+++ src/N5.Challenge.Infrastructure/N5DBContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N5.Challenge.Domain.Entities;
+
+namespace N5.Challenge.Infrastructure
+{
+    public class N5DBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<PermissionType> PermissionTypes { get; set; }
+
+
+        public N5DBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Employee>().HasKey(e => e.Id);
+            modelBuilder.Entity<Employee>().OwnsOne(x => x.Name);
+            modelBuilder.Entity<Employee>().OwnsOne(x => x.LastName);
+
+
+            modelBuilder.Entity<PermissionType>().HasKey(e => e.Id);
+            modelBuilder.Entity<PermissionType>().OwnsOne(x => x.Name);
+
+            modelBuilder.Entity<Permission>().HasKey(e => e.Id);
+
+            modelBuilder.Entity<Employee>().HasMany(o => o.Permissions)
+                .WithOne(o => o.Employee).HasForeignKey(o => o.EmployeeId);
+
+            modelBuilder.Entity<PermissionType>().HasMany(o => o.Permissions)
+                .WithOne(o => o.PermissionType).HasForeignKey(o => o.PermissionTypeId);
+        }
+    }
+}
---
+++ src/N5.Challenge.Infrastructure/Repository/EmployeeRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using N5.Challenge.Domain.Entities;
+using N5.Challenge.Domain.Repositories;
+using N5.Challenge.Domain.ValueObjects.Employee;
+
+namespace N5.Challenge.Infrastructure.Repository
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly N5DBContext _dbContext;
+
+        public EmployeeRepository(N5DBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IList<Employee>> GetAllAsync()
+            => await _dbContext.Employees.ToListAsync();
+
+        public async Task<Employee> GetByKeyAsync(EmployeeId key)
+            => await _dbContext.Employees.FindAsync(key.Value);
+
+        public async Task PostAsync(Employee entity)
+        {
+            _dbContext.Employees.Add(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task PutAsync(Employee entity)
+        {
+            var entityToUpdate = await _dbContext.Employees.FindAsync(entity.Id);
+            if (entityToUpdate is null)
+                throw new ArgumentNullException($"Employee {entity.Id} not exist");
+            _dbContext.Employees.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
